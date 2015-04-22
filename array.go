759c7b121bbd5de4b8dd361e7325ff64a5fda8eb
Agnesi <seuxl1@163.com>
2015-04-22 10:57:05 +0800

    Update array.go

--- array.go
+++ array.go
@@ -1,7 +1,7 @@
 package goquery
 
 import (
-	"github.com/agnesis/goquery/exp/html"
+	"github.com/lynx-seu/goquery/exp/html"
 )
 
 // First() reduces the set of matched elements to the first in the set.
